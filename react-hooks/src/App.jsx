a850f9df3307955746f2a8b0884748861da99bca
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-12 15:14:28 +0545

    learned data fetching

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { FaStar } from "react-icons/fa";
+// import React, { useState } from "react";
+// import ReactDOM from "react-dom";
+// import { FaStar } from "react-icons/fa";
 
-const createArray = (length) => [...Array(length)];
+// const createArray = (length) => [...Array(length)];
 
-function Star({ selected = false, onSelect }) {
-  return (
-    <FaStar
-      color={selected ? "red" : "grey"}
-      onClick={onSelect}
-      style={{ cursor: "pointer" }}
-    />
-  );
-}
+// function Star({ selected = false, onSelect }) {
+//   return (
+//     <FaStar
+//       color={selected ? "red" : "grey"}
+//       onClick={onSelect}
+//       style={{ cursor: "pointer" }}
+//     />
+//   );
+// }
 
-function StarRating({ totalStars = 5 }) {
-  const [selectedStar, setSelectedStar] = useState(0);
+// function StarRating({ totalStars = 5 }) {
+//   const [selectedStar, setSelectedStar] = useState(0);
 
-  return (
-    <div>
-      {createArray(totalStars).map((_, i) => (
-        <Star
-          key={i}
-          selected={selectedStar > i}
-          onSelect={() => setSelectedStar(i + 1)}
-        />
-      ))}
-      <p>{selectedStar} of {totalStars} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {createArray(totalStars).map((_, i) => (
+//         <Star
+//           key={i}
+//           selected={selectedStar > i}
+//           onSelect={() => setSelectedStar(i + 1)}
+//         />
+//       ))}
+//       <p>{selectedStar} of {totalStars} stars</p>
+//     </div>
+//   );
+// }
 
-ReactDOM.render(<StarRating totalStars={5} />, document.getElementById("root"));
+// ReactDOM.render(<StarRating totalStars={5} />, document.getElementById("root"));
 
 // function App() {
 //   const [status, setStatus] = useState("Not Delivered");
@@ -56,8 +56,44 @@ ReactDOM.render(<StarRating totalStars={5} />, document.getElementById("root"));
 //   )
 // }
 
-function App() {
-  return <StarRating totalStars={15} />;
-}
+// function App() {
+//   return <StarRating totalStars={15} />;
+// }
 
-export default App;
+// export default App;
+
+import React from "react";
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import { FaStar } from "react-icons/fa";
+
+// Home Rating Function 
+
+const createArray = (length)=>[...Array(length)]
+
+function Star({selected=false,onSelect}){
+  <FaStar 
+  color={selected?"yellow":"grey"}
+  onClick={onSelect}
+  />
+
+  function StarRating({totalStars=5}){
+    const [selectedStar,setSelectedStar]=useState(0);
+
+    return (
+      <div>
+           createArray(totalStars).map((n,i)=>(
+      <Star
+      key={i}
+      selected={selectedStar>i}
+      onSelect={()=>setSelectedStar(i+1)}
+      />
+    ))
+    <p>{totalStars} of {selectedStar}</p>
+  </div>
+    )
+  }
+  
+ 
+
+}
