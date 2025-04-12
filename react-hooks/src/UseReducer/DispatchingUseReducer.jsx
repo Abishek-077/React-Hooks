c58f2eb40e77247efd39e47846614ef9376979ad
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-12 16:41:55 +0545

    Debugging Mistakes

--- react-hooks/src/UseReducer/DispatchingUseReducer.jsx
+++ react-hooks/src/UseReducer/DispatchingUseReducer.jsx
@@ -1,42 +1,34 @@
-import React from "react";
-import { useReducer } from "react";
+import React, { useReducer } from "react";
 import "./index.css";
 
 const initialState = {
-    message: "hey"
-}
+    message: "Hey",
+};
 
 function reducer(state, action) {
     switch (action.type) {
-        case "sunday":
-            return {
-                message: "Push Day"
-            }
+        case "Sunday":
+            return { message: "Push Day" };
         case "Monday":
-            return {
-                message: "Pull Day"
-            }
+            return { message: "Pull Day" };
         case "Tuesday":
-            return {
-                message: "Leg Day"
-            }
+            return { message: "Leg Day" };
+        default:
+            return state;
     }
 }
 
-
 function DispatchingUseReducer() {
-    cons[state, dispatch] = useReducer(
-        reducer,
-        initialState
-    );
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     return (
         <>
-            <p>Mesage:{state.message}</p>
+            <p>Message: {state.message}</p>
             <button onClick={() => dispatch({ type: "Sunday" })}>Sunday</button>
             <button onClick={() => dispatch({ type: "Monday" })}>Monday</button>
             <button onClick={() => dispatch({ type: "Tuesday" })}>Tuesday</button>
-
         </>
-    )
+    );
 }
+
+export default DispatchingUseReducer;
